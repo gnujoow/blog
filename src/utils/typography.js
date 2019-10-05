8914fa651fa407c0fabd96be626fe590c241780e
gnujoow <gnujoow.kim@gmail.com>
2019-10-05 17:09:24 +0900

    darktheme
    https://github.com/KyleAMathews/typography.js/blob/master/packages/typography-theme-fairy-gates/src/index.js
    https://material.io/design/color/dark-theme.html#ui-application

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,6 +3,25 @@ import theme from 'typography-theme-fairy-gates'
 import CodePlugin from 'typography-plugin-code'
 
 theme.plugins = [new CodePlugin()]
+theme.overrideThemeStyles = ({ rhythm }, options) => ({
+  'h1,h2,h3': {
+    color: '#fff',
+  },
+  'p': {
+    color: 'rgba(255, 255, 255, 0.87)'
+  },
+  'a': {
+    color: '#BB86FC',
+    'text-shadow': 'none',
+  },
+  'blockquote': {
+    color: '#fff',
+    background: '#2d2d2d',
+    padding: '1rem',
+    'border-radius': '3px',
+  },
+})
+
 const typography = new Typography(theme)
 
 // Hot reload typography in development.
